8f0959441c38189c5fc423991b32e0ed561bb2e6
unknown <oalinares.jr@gmail.com>
2022-11-11 14:43:34 -0600

    Week 10 Coding Assingment commit

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -13,9 +13,12 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 		private Scanner scanner = new Scanner(System.in);
 		private ProjectService projectService = new ProjectService();
+		private Project curProject;
 	// @formatter:off
 		private List<String> operations = List.of(
-				"1) Add a project"
+				"1) Add a project",
+				"2) List projects",
+				"3) Select a project"
 		);
 		// @formatter:on
 
@@ -37,13 +40,39 @@ public class ProjectsApp {
 					case 1:
 						createProject();
 						break;
+					case 2:
+						listProjects();
+						break;
+					case 3:
+						selectProject();
+						break;
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
 					}
 				} catch (Exception e) {
 					System.out.println("\nError: " + e + " Try again.");
+					
 				}
 			}
+		}
+		private void selectProject() {
+			listProjects();
+			Integer projectId = getIntInput("Enter a project ID to select a project");
+			curProject = null;
+			curProject = projectService.fetchProjectById(projectId);
+		}
+		/*
+		 * The listProjects() will list all the projects that have been created by the user and/or already stored in the database.
+		 * The method should print the project's ID and name.
+		 */
+		private void listProjects() {
+			List<Project> projects = projectService.fetchAllProjects();
+			
+			System.out.println("\nProjects:");
+			
+			projects.forEach(project -> 
+			System.out.println("   " + project.getProjectId() 
+			+ ": " + project.getProjectName()));
 		}
 		private void createProject() {
 			String projectName = getStringInput("Enter the project name");
@@ -104,6 +133,12 @@ public class ProjectsApp {
 		private void printOperations() {
 			System.out.println("\nThese are the available selections. Press the Enter key to quit: ");
 			operations.forEach(line -> System.out.println(" " + line));
+			
+			if(Objects.isNull(curProject)) {
+				System.out.println("\nYou are not working with a project.");
+			} else {
+				System.out.println("\nYou are working with project: " + curProject);
+			}
 		}
 
 }
